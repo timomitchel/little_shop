f32a82b76b5f58e25e9ed1cc87bde1e0d4af8824
Kelley Jenkins <ckjenkins3@gmail.com>
2018-01-02 16:32:53 -0700

    creating login routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
 
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
 
-
+  resources :users, only:[:new, :create, :show]
+  
   resources :items, only: [:index]
 
   resources :categories, :path => "/", :only => [:show]
