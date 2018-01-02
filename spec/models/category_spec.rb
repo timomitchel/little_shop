5dbdd7e5c30e562d47769f4fa42298c0536cc29d
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-02 14:01:20 -0700

    Category spec test for validations passes

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe Category, type: :model do
     it "cannot save a valid category without a title" do
       category = Category.new()
 
-      expect(category).to_be invalid
+      expect(category).to be_invalid
     end
 
     it "can save a valid category with a title" do
       category = Category.new(title: "Coffee")
 
-      expect(category).to_be valid
+      expect(category).to be_valid
     end
   end
 end
