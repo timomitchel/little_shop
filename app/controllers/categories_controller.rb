e09eca11a6b18f22c2dc7c3b1729435560cefbfe
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-02 15:19:29 -0700

    User can visit category itesm spec test passes, slug user story passes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
 
   def show
-    @category = Category.find_by(title: params[:slug])
+    @category = Category.find_by_title(params[:id])
   end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,9 @@
 class Category < ApplicationRecord
   validates :title, presence: true, uniqueness: true
 
-  has_many :items 
+  has_many :items
+
+  def to_param
+    title
+  end
 end
