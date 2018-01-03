6c370709df36664e8cd3854d1240238e0e4361d5
Kelley Jenkins <ckjenkins3@gmail.com>
2018-01-03 14:24:45 -0700

    creating new users and session dashboard path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      redirect_to dashboard_path
     else
       render :new
     end
