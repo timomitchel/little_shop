e570d71698189012ad3a633f957bd00615a4bbfd
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-03 14:58:32 -0700

    Build out carts controller create method

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,9 @@
+class CartsController < ApplicationController
+
+  def create
+    item = Item.find(params[:item_id])
+    flash[:notice] = "You now have 1 #{item.title} in your cart!"
+    redirect_to items_path
+  end
+
+end
