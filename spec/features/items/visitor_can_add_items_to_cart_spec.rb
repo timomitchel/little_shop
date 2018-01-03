4a5ee0133a3cfbac0260c8cc45fcbc4d5cc74824
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-03 15:27:05 -0700

    Feature test for cart tracker implementation and cart tracker implementation in application.html.erb

--- spec/features/items/visitor_can_add_items_to_cart_spec.rb
+++ spec/features/items/visitor_can_add_items_to_cart_spec.rb
@@ -19,5 +19,20 @@ describe "As a visitor" do
 
       expect(page).to have_content("You now have 2 #{@item.title}s in your cart!")
     end
+
+    it ' also displays the total number of items in the cart incrementally' do
+
+      visit items_path
+
+      expect(page).to have_content("Cart: 0")
+
+      click_button("Add to Cart")
+
+      expect(page).to have_content("Cart: 1")
+
+      click_button("Add to Cart")
+
+      expect(page).to have_content("Cart: 2")
+    end
   end
 end
