9766edb75688e5e404e060472c1b64e4190b9a22
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-03 14:58:59 -0700

    Build out basi cfeature test for visitor add item to cart spec, test passes, but will need to make more robust

---
+++ spec/features/items/visitor_can_add_items_to_cart_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe "As a visitor" do
+  context "I can place items in a cart" do
+    before :each do
+      @category = create(:category)
+      @item = create(:item, image: "latte.jpg", category:@category)
+
+    end
+
+    it "when I go to items index, it displays an updated cart total and displays a flash message" do
+      visit items_path
+
+      click_button("Add Item")
+
+      expect(page).to have_content("You now have 1 #{@item.title} in your cart!")
+    end
+  end
+end
