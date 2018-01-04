8b2f146429c97121cf900b17c932590d73908f21
Tim Tyrrell <timomitchel23@gmail.com>
2018-01-04 14:45:41 -0700

    indentation

--- spec/features/users/user_can_create_new_user_spec.rb
+++ spec/features/users/user_can_create_new_user_spec.rb
@@ -5,16 +5,16 @@ describe "User can sign up as a new user" do
     context "user clicks link to Create Account" do
       it "creates a new user" do
 
-    visit new_user_path
+        visit new_user_path
 
-    fill_in "user[username]", with: "JavaJoe"
-    fill_in "user[password]", with: "joe123"
-    click_button "Create Account"
+        fill_in "user[username]", with: "JavaJoe"
+        fill_in "user[password]", with: "joe123"
+        click_button "Create Account"
 
-    expect(current_path).to eq(dashboard_path)
-    expect(page).to have_content("Logged in as #{User.last.username}!")
-    expect(page).to_not have_content("Login")
-    expect(page).to have_content("Log Out")
+        expect(current_path).to eq(dashboard_path)
+        expect(page).to have_content("Logged in as #{User.last.username}!")
+        expect(page).to_not have_content("Login")
+        expect(page).to have_content("Log Out")
       end
     end
   end
