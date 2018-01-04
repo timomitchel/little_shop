0113f28dfac7159fa2dcc989d8a1b947efa18ea3
Kelley Jenkins <ckjenkins3@gmail.com>
2018-01-04 16:57:12 -0700

    test that items remain in cart after visitor creates account

--- spec/features/carts/visitor_can_login_create_acct_from_cart_spec.rb
+++ spec/features/carts/visitor_can_login_create_acct_from_cart_spec.rb
@@ -16,4 +16,27 @@ describe "A guest visitor can log in or create accout from cart page" do
           expect(page).to have_content("Create Account to Checkout")
     end
   end
+  context "user creates a new account" do
+    it "items in cart are still in cart" do
+      category = Category.create(title: "coffee")
+      coffee_1 = Item.create(title: "French Roast", description: "1 Pound of Deliciousio!", price: "$2.95", image: "latte.jpg",  category: category)
+
+      visit items_path
+      click_on "Add to Cart"
+      visit '/cart'
+      click_on "View Cart"
+      click_on "Create Account to Checkout"
+
+      fill_in "user[username]", with: "JavaJoe"
+      fill_in "user[password]", with: "joe123"
+      click_button "Create Account"
+
+      visit '/cart'
+
+      expect(page).to have_content(coffee_1.title)
+      expect(page).to have_content(coffee_1.description)
+      expect(page).to have_content(coffee_1.price)
+      expect(page).to have_link("Checkout")
+    end
+  end
 end
