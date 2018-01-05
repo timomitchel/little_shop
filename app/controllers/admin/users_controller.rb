11347fd0404fc86a7420964298bf5863dfc8e957
Kelley Jenkins <ckjenkins3@gmail.com>
2018-01-04 20:26:33 -0700

    creating admin dashboard view and controller

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,5 @@
+class Admin::UsersController < ApplicationController
+
+  def show
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,11 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to dashboard_path
+      unless current_user.admin?
+        redirect_to dashboard_path
+      else
+        redirect_to admin_dashboard_path
+      end
     else
       render :new
     end
