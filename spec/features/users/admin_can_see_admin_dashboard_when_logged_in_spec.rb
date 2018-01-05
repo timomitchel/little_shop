c4c5bc205e8da1e575e476f3b0ffd07c524f6eef
Kelley Jenkins <ckjenkins3@gmail.com>
2018-01-04 20:47:11 -0700

    fixing indents

--- spec/features/users/admin_can_see_admin_dashboard_when_logged_in_spec.rb
+++ spec/features/users/admin_can_see_admin_dashboard_when_logged_in_spec.rb
@@ -18,14 +18,16 @@ describe "User who is an admin logs in and sees admin dashboard" do
       expect(page).to have_content("Admin Dashboard")
     end
   end
+  
   context "a default user logs in" do
     it 'does not allow default user to see admin categories index' do
-    user = create(:user, role: 0)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      user = create(:user, role: 0)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit admin_dashboard_path
-    expect(page).to_not have_content("Admin Dashboard")
-    expect(page).to have_content("The page you were looking for doesn't exist.")
+      visit admin_dashboard_path
+
+      expect(page).to_not have_content("Admin Dashboard")
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
   end
-end
 end
