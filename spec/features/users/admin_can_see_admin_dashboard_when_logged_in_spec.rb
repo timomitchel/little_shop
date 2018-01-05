15bbc2707e930cfb6c8eeb8002123482290cedad
Kelley Jenkins <ckjenkins3@gmail.com>
2018-01-04 20:44:32 -0700

    regular user can not see admin dashboard test

--- spec/features/users/admin_can_see_admin_dashboard_when_logged_in_spec.rb
+++ spec/features/users/admin_can_see_admin_dashboard_when_logged_in_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "Admin logs in and sees admin dashboard" do
+describe "User who is an admin logs in and sees admin dashboard" do
   context "an admin logs in" do
     it "they see Admin Dashboard" do
       admin = create(:user, role: 1)
@@ -18,4 +18,14 @@ describe "Admin logs in and sees admin dashboard" do
       expect(page).to have_content("Admin Dashboard")
     end
   end
+  context "a default user logs in" do
+    it 'does not allow default user to see admin categories index' do
+    user = create(:user, role: 0)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit admin_dashboard_path
+    expect(page).to_not have_content("Admin Dashboard")
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+  end
+end
 end
