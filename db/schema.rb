d8be7f4e2ca0e8eb08d8d117bba5de34e39c9475
Kelley Jenkins <ckjenkins3@gmail.com>
2018-01-04 20:22:42 -0700

    created role migration for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103194057) do
+ActiveRecord::Schema.define(version: 20180105023001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180103194057) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.integer "role", default: 0
   end
 
   add_foreign_key "items", "categories"
