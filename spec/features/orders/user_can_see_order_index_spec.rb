87db206e74d603a31a908707687ae4f54c018dae
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-06 14:12:33 -0700

    Update feature test taking out methods taht dont need to exist

--- spec/features/orders/user_can_see_order_index_spec.rb
+++ spec/features/orders/user_can_see_order_index_spec.rb
@@ -8,23 +8,25 @@ describe "As a user" do
       @cart = Cart.new(@item.id => 1)
       @user = User.create(username:"TYJ", password: "123", role: 0)
 
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
     end
 
     it "displays an index of all user orders organized by order ID" do
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    visit cart_path
 
     expect(current_path).to eq(cart_path)
+    
+    click_on("Checkout")
 
-    click_on("Check Out")
+    order = Order.create(status: "Paid", created_at: "2017-08-09",user_id: @user.id)
 
-    order = Order.create(status_code: "Paid", total_price: @cart.total_price, date_time: "2017-08-09")
-
-    ItemOrders.create(item_id: @item.id, order_id: order.id)
+    ItemOrder.create(item_id: @item.id, order_id: order.id)
 
     expect(current_path).to eq(user_orders_path(@user))
     expect(page).to have_content("All Orders for #{@user.username}")
-    expect(page).to have_content("#{order.id} placed on #{order.date_time}")
+    expect(page).to have_content("#{order.id} placed on #{order.created_at}")
   end
 end
 end
