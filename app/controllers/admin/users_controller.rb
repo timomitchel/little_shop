3d73085b0d61fa055d0a12ae49aae64164b4050c
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-07 14:10:19 -0700

    create Admin Users Controller action show, edit, update with strong params

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,23 @@
 class Admin::UsersController < Admin::BaseController
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    current_user.update(user_params)
+
+    redirect_to admin_dashboard_path
+  end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :password, :fullname, :address)
+    end
 end
