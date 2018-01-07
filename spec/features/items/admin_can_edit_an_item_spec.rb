e94a8dfaf1227033cc28abd19c7e84e5c7809c28
Kelley Jenkins <ckjenkins3@gmail.com>
2018-01-07 13:47:13 -0700

    test for editing and item

---
+++ spec/features/items/admin_can_edit_an_item_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "Admin can edit an existing item" do
+  scenario "an admin can edit items" do
+
+  admin = create(:user, role: 1)
+  allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+  category = create(:category)
+  item = create(:item, category: category)
+
+  visit edit_admin_item_path(item)
+
+  fill_in "item[title]", with: "New Item"
+  click_button "Update Item"
+
+  expect(page).to have_content("New Item")
+  end
+end
