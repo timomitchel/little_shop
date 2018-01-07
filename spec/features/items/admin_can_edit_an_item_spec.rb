2e59ac24122ae537e203029593e7cf053f5d8dfb
Kelley Jenkins <ckjenkins3@gmail.com>
2018-01-07 14:05:37 -0700

    fixing edit test so it will save item

--- spec/features/items/admin_can_edit_an_item_spec.rb
+++ spec/features/items/admin_can_edit_an_item_spec.rb
@@ -11,6 +11,9 @@ describe "Admin can edit an existing item" do
   visit edit_admin_item_path(item)
 
   fill_in "item[title]", with: "New Item"
+  fill_in "item[description]", with: "New Description"
+  fill_in "item[price]", with: 7.4
+
   click_button "Update Item"
 
   expect(page).to have_content("New Item")
