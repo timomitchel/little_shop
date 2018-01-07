47f10f129f7458b4e505fb5944251017bb62ade9
Kelley Jenkins <ckjenkins3@gmail.com>
2018-01-07 13:48:43 -0700

    adding edit and update methods to controller

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,9 +11,23 @@ class Admin::ItemsController < Admin::BaseController
     @item = Item.new(item_params)
     if @item.save
       flash[:success] = "You created the #{@item.title}!"
-      redirect_to items_path
+      redirect_to admin_items_path
     else
         render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.update(params[:id], item_params)
+    if @item.save
+      flash[:success] = "#{@item.title} updated!"
+      redirect_to admin_items_path
+    else
+      render :edit
     end
   end
 
