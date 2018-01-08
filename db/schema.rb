3268edd2bb0bfb5f363a07320297aa5e3778d569
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-08 13:56:25 -0700

    add migration file, plus schema updated and admin folder under spec features

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180108185305) do
-
+ActiveRecord::Schema.define(version: 20180108204235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,8 +52,8 @@ ActiveRecord::Schema.define(version: 20180108185305) do
     t.string "username"
     t.string "password_digest"
     t.integer "role", default: 0
-    t.string "fullname"
     t.string "address"
+    t.string "fullname"
   end
 
   add_foreign_key "item_orders", "items"
---
+++ spec/features/admin/admin_sees_dashboard_with_status_and_filters_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+describe "User who is an admin logs in and sees admin dashboard" do
+  context "an admin logs in" do
+    context "they click on admin dashboard" do
+    before(:each) do
+      @admin = create(:user, role: 1)
+      @user = create(:user, role: 0)
+      @user_2 = create(:user, role: 0)
+      order_1 = create(:order, status: 0, user_id: @user.id)
+      order_2 = create(:order, status: 1, user_id: @user.id)
+      order_3 = create(:order, status: 2, user_id: @user.id)
+      order_4 = create(:order, status: 3, user_id: @user.id)
+      order_5 = create(:order, status: 0, user_id: @user_2.id)
+      order_6 = create(:order, status: 2, user_id: @user_2.id)
+    end
+    it "they see Admin Dashboard" do
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+        visit admin_dashboard_path
+
+        expect(page).to have_content ("Orders:")
+        expect(page).to have_content ("Totals:")
+        expect(page).to have_content ("2")
+        expect(page).to have_content ("1")
+
+        expect(page).to have_button "cancel"
+        expect(page).to have_button "mark as paid"
+        expect(page).to have_button "mark as completed"
+        expect(page).to have_link "Ordered"
+        expect(page).to have_link "Paid"
+        expect(page).to have_link "Cancelled"
+        expect(page).to have_link "Completed"
+        expect(page).to have_link "All"
+      end
+
+      it 'also displays a link to analytics dashboard, that redirects admin to /admin/analytics-dashboard' do
+        click_link "Analytics Dashboard"
+
+        expect(current_path).to eq("/admin/analytics-dashboard")
+
+        expect(page).to have_content("Analytics Dashboard")
+      end
+    end
+  end
+end
