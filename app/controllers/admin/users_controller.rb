36084cefceab0da8f430f3652091f7d6c20466e0
Tim Tyrrell <timomitchel23@gmail.com>
2018-01-08 12:49:03 -0700

    fixed mark as complete buttons and mark as paid buttons to render correctly

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,24 @@ class Admin::UsersController < Admin::BaseController
   def show
     @orders = Order.all
   end
+
+  def destroy
+    order = Order.find(params[:order_id])
+    order.delete
+    redirect_to admin_dashboard_path
+  end
+
+  def update
+    order = Order.find(params[:order_id])
+    if params[:update_type] == "complete"    
+      order.update_status_complete
+      redirect_to admin_dashboard_path
+    elsif params[:update_type] == "paid"
+      order.update_status_paid
+      redirect_to admin_dashboard_path
+    else
+      flash[:error] = "could not update status"
+      redirect_to admin_dashboard_path
+    end
+  end
 end
