71943a2a7a9480e4bff86e9f62e603d231ea20f9
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-09 14:11:37 -0700

    Add link to go to orders from user dashbaord, create feaeture test, feature test passes

--- spec/features/orders/user_can_see_order_index_spec.rb
+++ spec/features/orders/user_can_see_order_index_spec.rb
@@ -34,11 +34,20 @@ describe "As a user" do
 
       click_on("Checkout")
 
-      
+
       expect(current_path).to eq(user_orders_path(@user))
       expect(page).to have_content("All Orders for #{@user.username}")
       expect(page).to have_content("#{@user.orders.last.id} placed on #{@user.orders.last.created_at}")
 
+      visit dashboard_path
+      
+      expect(page).to have_content("Go to your Orders")
+
+      click_link "Go to your Orders"
+
+      expect(current_path).to eq(user_orders_path(@user))
+
+      expect(page).to have_content("All Orders for #{@user.username}")
     end
 end
 end
