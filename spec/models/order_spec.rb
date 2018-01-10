9f5b21a0095d17c3ef9cf563c16a5f8b48766c54
Tim Tyrrell <timomitchel23@gmail.com>
2018-01-09 19:59:25 -0700

    class methods of order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
 describe Order do
+  before(:each) do
+    @user = create(:user)
+    @order = Order.create(user: @user)
+    @order_2 = Order.create(status: 1, user: @user)
+    @order_3 = Order.create(status: 2, user: @user)
+    @order_4 = Order.create(status: 3, user: @user)
+    @order_5 = Order.create(status: 1, user: @user)
+    @order_6 = Order.create(status: 2, user: @user)
+  end
   context "relationships" do 
-    it { should belong_to(:user)}
+    it {should belong_to(:user)}
     it {should have_many(:item_orders)}
     it {should have_many(:items)}
   end
@@ -11,6 +20,11 @@ describe Order do
     
   end
   context "class methods" do 
-
+    it "responds to all status count methods" do 
+      expect(Order.ordered_count).to eq 1
+      expect(Order.paid_count).to eq 2
+      expect(Order.cancelled_count).to eq 2
+      expect(Order.completed_count).to eq 1
+    end
   end
 end
